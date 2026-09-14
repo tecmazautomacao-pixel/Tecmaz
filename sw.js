e9d0a0dbdbecc2e6911a4a44d883a08654d264b5
tecmazautomacao-pixel <tecmazautomacao@gmail.com>
2026-09-14 14:14:03 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,5 @@
-const CACHE = "tecmaz-v2";
-
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+const CACHE = "tecmaz-online-v1";
+const ASSETS = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+self.addEventListener("install", e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate", e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("fetch", e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
